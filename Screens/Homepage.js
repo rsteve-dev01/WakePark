bf988c3f7ccc2dc7045372eeac7eeb1b8c531d77
kutucodes <shreya-mukherjee@hotmail.com>
2024-03-01 20:37:57 -0500

    Update

--- Screens/Homepage.js
+++ Screens/Homepage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import { View, Text } from 'react-native';
 
 export const HomePage = ({ navigation }) => {
@@ -8,5 +8,36 @@ export const HomePage = ({ navigation }) => {
         </View>
     );
 }
+
+export default HomePage;
+*/
+
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+export const HomePage = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <Text>Homepage</Text>
+            <Image
+                source={require('/Users/shreyamukherjee/Documents/WakeTech/Spring_2024/CSC289/WakePark/images/homepage_pic.png')}
+                style={styles.image}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'top',
+    },
+    image: {
+        width: 400,
+        height: 400,
+        resizeMode: 'contain',
+    },
+});
 
 export default HomePage;
