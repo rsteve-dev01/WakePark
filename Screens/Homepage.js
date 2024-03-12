149708b1ebe41ee49394f14a6de9ff66165dffcf
3osmic <aliyah.y.lewis@gmail.com>
2024-03-12 12:38:21 -0400

    Updates

--- Screens/Homepage.js
+++ Screens/Homepage.js
@@ -1,49 +1,22 @@
 import React from 'react';
-import { View, TouchableOpacity, ImageBackground, Image, Linking } from 'react-native';
-import { styles } from '../StyleSheet';
-import WakeTechImage from '../assets/WakeTech.png'; // Adjust the path based on the location of WakeTech.png
-/* import React from 'react';
-import { View, Text } from 'react-native';
->>>>>>> Navigation-Integration
-
-export const HomePage = ({ navigation }) => {
-    const handlePress = () => {
-        Linking.openURL('https://www.waketech.edu/wake-tech-news');
-    };
-
-    return (
-        <ImageBackground source={require('../assets/Background.jpg')} style={styles.backgroundImage}>
-            <View style={styles.container}>
-                <TouchableOpacity onPress={handlePress}>
-                    <Image source={WakeTechImage} style={styles.logo} />
-                </TouchableOpacity>
-            </View>
-        </ImageBackground>
-    );
-}
-
-export default HomePage;export default HomePage;
-*/
-
-import React from 'react';
 import { View, Text, Image, ImageBackground, StyleSheet } from 'react-native';
 
-
-export const HomePage = ({ navigation }) => {
+const HomePage = ({ navigation }) => {
     return (
         <ImageBackground
-            source={require('/Users/shreyamukherjee/Documents/WakeTech/Spring_2024/CSC289/WakePark/assets/Background.jpg')} 
+            source={require('../assets/Background.jpg')} 
             style={styles.background}>
             <View style={styles.container}>
-                <Image source={require('/Users/shreyamukherjee/Documents/WakeTech/Spring_2024/CSC289/WakePark/images/homepage_pic.png')}
-                style={styles.image}/>
-            <Text>Cheapest and Closest Parking Spots</Text>
-            <Text>Contact Us</Text>
+                <Image
+                    source={require('../images/homepage_pic.png')}
+                    style={styles.image}
+                />
+                <Text>Cheapest and Closest Parking Spots</Text>
+                <Text>Contact Us</Text>
             </View>
         </ImageBackground>
-        
     );
-}
+};
 
 const styles = StyleSheet.create({
     background: {
@@ -64,4 +37,3 @@ const styles = StyleSheet.create({
 });
 
 export default HomePage;
-
